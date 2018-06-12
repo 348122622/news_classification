b6c23b15d0b907123e4f0b37a79455b974a9c721
yaozhuy <348122622@qq.com>
2018-06-12 22:53:03 +0800

    1.bow, tf-idf分离;2.下一步加入n-gram

--- Scripts/train.py
+++ Scripts/train.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import os
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
-from utils import get_train_val
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from Scripts.data_helper import get_clean_data
+from Scripts.utils import data_to_bow, data_to_tfidf, split_train_val
 
 
 def train(data):
@@ -21,13 +22,24 @@ def train(data):
 
 
 def eval(y, y_pred):
-    print("acc: {}, f1_score: {}".format(accuracy_score(y, y_pred), f1_score(y, y_pred)))
+    print("precision: {}\nrecall: {}\nf1_score: {}".format(precision_score(y, y_pred),
+                                                           recall_score(y, y_pred),
+                                                           f1_score(y, y_pred)))
     print("train confusion matrix:")
     print(confusion_matrix(y, y_pred))
 
 
 if __name__ == '__main__':
-    base_dir = '../dataSet/IMDB'
-    ftrain = 'labeledTrainData.tsv'
-    data = get_train_val(os.path.join(base_dir, ftrain), tfidf=False)
-    cls = train(data)
+    data_dir = os.path.join(os.path.abspath('..'), 'data')
+    tech_path = os.path.join(data_dir, 'tech_data', 'tech_train.csv')
+    clean_content, labels = get_clean_data(tech_path, train=True)
+    # data_features, vectorizer = data_to_bow(clean_content)
+    data_features, vectorizer = data_to_tfidf(clean_content)
+    x_train, y_train, x_val, y_val = split_train_val(data_features, labels, split_rate=0.8)
+    rf = train((x_train, y_train, x_val, y_val))
+
+    tech_test_path = os.path.join(data_dir, 'tech_data', 'tech_test.csv')
+    clean_content_test, labels_test = get_clean_data(tech_test_path, train=True)
+    data_features_test = vectorizer.transform(clean_content_test).toarray()
+    labels_pred = rf.predict(data_features_test)
+    eval(labels_test, labels_pred)
--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,35 +1,44 @@
 # coding=utf-8
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
-def data_to_bow(clean_content, tfidf=False):
+def data_to_bow(clean_content, max_text_len=5000):
     vectorizer = CountVectorizer(analyzer="word",
                                  tokenizer=None,
                                  preprocessor=None,
                                  stop_words=None,
-                                 max_features=5000)
+                                 max_features=max_text_len)
     data_features = vectorizer.fit_transform(clean_content)
     print("feature extracted.")
-    if tfidf is True:
-        transformer = TfidfTransformer()
-        data_features = transformer.fit_transform(data_features)
+    # if tfidf is True:
+    #     transformer = TfidfTransformer()
+    #     data_features = transformer.fit_transform(data_features)
+    return data_features.toarray(), vectorizer
+
+
+def data_to_tfidf(clean_content, max_text_len=5000):
+    vectorizer = TfidfVectorizer(analyzer="word",
+                                 tokenizer=None,
+                                 preprocessor=None,
+                                 stop_words=None,
+                                 max_features=max_text_len)
+    data_features = vectorizer.fit_transform(clean_content)
+    print("feature extracted.")
     return data_features.toarray(), vectorizer
 
 
 def split_train_val(data_features, labels, split_rate=0.8):
-    total_size = data_features.shape[0]
-    shuffle = np.random.permutation(total_size)
-    data_features = data_features[shuffle]
-    labels = np.array(labels[shuffle])
-    train_size = int(total_size * split_rate)
+    ss = StratifiedShuffleSplit(n_splits=1, test_size=1 - split_rate, random_state=0)
     print("validate set splited.")
-    return data_features[:train_size], labels[:train_size], \
-           data_features[train_size:], labels[train_size:]
+    for train_index, val_index in ss.split(data_features, labels):
+        return data_features[train_index], labels[train_index], \
+               data_features[val_index], labels[val_index]
 
 
 # def get_train_val(filepath, split_rate=0.8, tfidf=False):
 #     clean_reviews, labels = get_clean_data(filepath, train=True)
-#     data_features, vectorizer = data_to_bow(clean_reviews, tfidf=Ture)
+#     data_features, vectorizer = data_to_bow(clean_reviews, tfidf=True)
 #     return split_train_val(data_features, labels, split_rate=0.8)
 
